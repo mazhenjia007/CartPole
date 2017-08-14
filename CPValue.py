384a346f0a01d65f438f4db1c67c0f94c70c32d7
mazhenjia <mazhenjia@hotmail.com>
2017-08-14 18:13:52 +0900

    Modify Evaluation

--- CPValue.py
+++ CPValue.py
@@ -13,46 +13,52 @@ def CPValue():
     row, col = Pms.shape
 
     Vs = np.zeros((row, 1))
+    ts = np.zeros((row, 1))
 
     hENV = CPENV.TCP_ENV()
     gamma = 0.95
 
-    maxIter = 1000
+    nIter = 100
     eps = 1e-6
 
     for i in range(row):
         perc = float(i) / float(row) * 100
-        sys.stdout.write("%d / %d, %.4f%%\r" % (i, row, perc))
+        ## sys.stdout.write("%d / %d, %.4f%%\r" % (i, row, perc))
+        sys.stdout.write("\r%.4f%%" % (perc))
 
         V = 0
-        V_past = V
+        t = 0
         flag = 1
         params = Pms[i][:]
         n = 0
 
         iIter = 0
-        while (flag==1 and iIter<maxIter):
-            delta, V_new = CPAGNT.ExecEpi(params, gamma, hENV)
-            
-            sys.stdout.write("%.4f\n" % (V_new))
-
+        while (flag==1 and iIter<nIter):
+            delta, V_new, t_new = CPAGNT.ExecEpi(params, gamma, hENV)
             if n==0:
-                V_past = V
                 V = V_new
+                t = t_new
                 n = 1
             else:
-                V_past = V
-                V = (n-1)/n*V + V_new/n
                 n = n + 1
+                V_past = V
+                t_past = t
+                V = (n-1)/n*V + V_new/n
+                t = (n-1)/n*t + t_new/n
+                if abs(V - V_past) < eps:
+                    flag = 0
+                if abs(t - t_past) < eps:
+                    flag = 0
+
             iIter = iIter + 1
-            
-            if abs(V - V_past) < eps:
-                flag = 0
 
         if flag == 0:
             Vs[i] = V
+            ts[i] = t
         else:
             sys.stdout.write("\nError!! Iteration do not convergence!!\n")
             return
 
-    plt.plot(Vs)
+    plt.plot(ts)
+
+CPValue()
